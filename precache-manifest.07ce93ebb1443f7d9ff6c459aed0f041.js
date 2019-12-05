868336ba26a47c3ff313de77f0ed5a8d9e5257e3
davecaos <david_bluetrain@hotmail.com>
2019-12-05 09:39:32 -0300

    Updates

---
+++ precache-manifest.07ce93ebb1443f7d9ff6c459aed0f041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5d577b58d8d95017d90d19fe324f2ad",
+    "url": "/alf-tarot/static/media/Balfomet2.e5d577b5.jpg"
+  },
+  {
+    "revision": "9dd18d076aa2c6e67d90",
+    "url": "/alf-tarot/static/js/runtime~main.279413d3.js"
+  },
+  {
+    "revision": "97cdf0d0f44c017154af",
+    "url": "/alf-tarot/static/js/main.439fc66f.chunk.js"
+  },
+  {
+    "revision": "8481f7c9ed47c9b02cd6",
+    "url": "/alf-tarot/static/js/2.b199cbbb.chunk.js"
+  },
+  {
+    "revision": "97cdf0d0f44c017154af",
+    "url": "/alf-tarot/static/css/main.27296e58.chunk.css"
+  },
+  {
+    "revision": "8481f7c9ed47c9b02cd6",
+    "url": "/alf-tarot/static/css/2.a9116913.chunk.css"
+  },
+  {
+    "revision": "c2e723936ad52ed0b20c78ea35881785",
+    "url": "/alf-tarot/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/alf-tarot/precache-manifest.cf8aca73790d1a6b15dff1d26310d75b.js"
+  "/alf-tarot/precache-manifest.07ce93ebb1443f7d9ff6c459aed0f041.js"
 );
 
 workbox.clientsClaim();
